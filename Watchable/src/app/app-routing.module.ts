915305d5e50196bb84caea4ae9ed70a484985f82
chris <revan2091@live.com>
2023-01-09 10:14:22 -0600

    message

---
+++ Watchable/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forRoot([
+    {path: '', component: HomeComponent},
+    {path: 'movies', component: MovieListComponent},
+    //{path: 'Movies/:movieId', component: MovieDetailsComponent},
+  ])],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
