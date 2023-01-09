45a76d0bb3c8477ed03de620f8419fd9159508ab
chris <revan2091@live.com>
2023-01-09 15:32:31 -0600

    Added basic routes and models for node backend

---
+++ Watchable/src/server/routes/movie_route.js
@@ -0,0 +1,20 @@
+
+const express = require("express");
+const movie_route = express.Router();
+const ObjectId = require("mongodb").ObjectId;
+const Movie = require("../models/movies");
+
+movie_route.get('/movies', async (req, res) => {
+  const response = await Movie.find()
+  res.json(response);
+});
+
+movie_route.get("/Movies/:MovieId",  (req, res) => {
+  Movie.findOne({_id: ObjectId(req.params.id)}, (err, result) =>{
+    if(err) throw err;
+    res.json(result);
+  });
+});
+
+
+module.exports = movie_route;
