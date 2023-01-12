f8e9908685cdc03e929a986a6821a921a9e7a288
chris <revan2091@live.com>
2023-01-12 16:04:58 -0600

    Completed search feature, large update to styles, added color scheming, new fonts, and completed implementation of API

--- Watchable/src/app/app.module.ts
+++ Watchable/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { HeaderComponent } from './header/header.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
 import { HomeComponent } from './home/home.component';
 import {HttpClientModule} from '@angular/common/http';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MovieListComponent,
-    HomeComponent
+    HomeComponent,
+    MovieDetailsComponent
   ],
   imports: [
     BrowserModule,
